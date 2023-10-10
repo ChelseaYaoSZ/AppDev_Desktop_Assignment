1a20091bd11b86b5b8d92bbb33dabefe83bab9a8
Shunzi Yao <2395028@edu.vaniercollege.qc.ca>
2023-10-10 10:36:32 -0400

    Added the first part

---
+++ assignment1/DatabaseConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    public static class DatabaseConfig
+    {
+        public static string GetConnectionString()
+        {
+            string host = "Host=localhost;";
+            string port = "Port=5432;";
+            string dbName = "Database=farmers_market;";
+            string userName = "Username=postgres;";
+            string password = "Password=133080;";
+
+            string connectionString = string.Format("{0}{1}{2}{3}{4}", host, port, dbName, userName, password);
+            return connectionString;
+        }
+    }
+}
